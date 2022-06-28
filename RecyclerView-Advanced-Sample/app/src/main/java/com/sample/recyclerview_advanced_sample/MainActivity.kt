410482b77657052ed2e895a949f8897074f09c78
jymun-97 <mjy0750@gmail.com>
2022-06-28 15:23:19 +0900

    init

---
+++ RecyclerView-Advanced-Sample/app/src/main/java/com/sample/recyclerview_advanced_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sample.recyclerview_advanced_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
